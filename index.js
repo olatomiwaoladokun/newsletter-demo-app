196e352a041018b559ad96cdfe53f5b642d0fdd9
olatomiwaoladokun <olatomiwaoladokun@gmail.com>
2023-09-12 13:35:13 +0100

    Implimented  the function to log the email Input

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 let emailInput = document.getElementById('email-input');
 let submitButton = document.getElementById('submit-button');
+
+// emailInput.oninput = logInput;
+
+submitButton.onclick = logInput;
+function logInput(){
+    console.log (emailInput.value);
+};
